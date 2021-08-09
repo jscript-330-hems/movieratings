a0ad7b3342561b2aa166f94823304ffadf9352c8
Eddie Lee <eddie@workbook.localdomain>
2021-08-09 15:50:30 -0700

    first commit - wip

--- models/review.js
+++ models/review.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  // password:  { type: String, required: true},
-  // email: { type: String, unique: true, required: true},
-  // roles: { type: [String], required: true }
+  movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'movie', required: true },
+  review:  { type: String, required: true },
 });
-
 
 module.exports = mongoose.model("reviews", reviewSchema);
