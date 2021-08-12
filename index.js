ed28b0a42d22b879aaf548f99e2a4d510a072472
Eddie Lee <eddie@workMini.localdomain>
2021-08-11 17:26:00 -0700

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 
-mongoose.connect($DB_HOST, {
+mongoose.connect('mongodb+srv://hems:hdjhmVi7esVA@cluster21.0hrdz.mongodb.net/movieratings?retryWrites=true&w=majority', {
   useNewUrlParser: true, 
   useCreateIndex: true, 
   useUnifiedTopology: true,
