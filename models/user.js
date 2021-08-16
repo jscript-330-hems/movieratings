ae3d2d9822d618365ea1ac2cdb9a3c598473af43
Mike Tate <shagee@uw.edu>
2021-08-16 07:25:20 -0700

    added test for movies endpoint

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   password:  { type: String, required: true},
   email: { type: String, unique: true, required: true},
+  username: { type: String, unique: true },
   roles: { type: [String], required: true }
 });
 
