9e594bb7898dfb76e94b43fc02d001e66a31b048
Mike Tate <shagee@uw.edu>
2021-08-16 19:41:18 -0700

    Update auth middleware based on comments from previous homework

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   password:  { type: String, required: true},
   email: { type: String, unique: true, required: true},
-  username: { type: String, unique: true },
   roles: { type: [String], required: true }
 });
 
